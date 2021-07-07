23b9875c7c7e8fcecaab4762869df4ece04d6f45
Patyval <pvaldezo@eafit.edu.com>
2021-07-07 00:22:28 +0000

    Jenkins

---
+++ devops.sh
@@ -0,0 +1,11 @@
+ssh-keygen -t ed25519 -C "pvaldezo@eafit.edu.co"
+cat ~/.ssh/config
+touch ~/.ssh/config
+cat > ~/.ssh/config
+Host *
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_ed25519
+Press Ctrl+D
+eval "$(ssh-agent)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
