b4babeafba93f35d8322a1d0e7935acbc4fdf557
patyval <86385553+patyval@users.noreply.github.com>
2021-07-27 18:24:44 -0500

    Update index.spec.js

--- module_6/tests/index.spec.js
+++ module_6/tests/index.spec.js
@@ -1,7 +1,9 @@
-const plus = require("./index")
-test(
-  "4",
-  () => {
-    expect (plus(2,2)).toBe(4)
-  }
-);
+const calculator = require("./index");
+
+test("Returns 4", () => {
+    expect(calculator.plus(2,2)).toBe(4);
+});
+
+test("Returns 0", () => {
+    expect(calculator.minus(2,2)).toBe(0);
+});
